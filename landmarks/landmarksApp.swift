f3374c5fa7a07873c2054f9187350602e73abd20
elfer07 <morenofernando@gmail.com>
2023-08-12 13:46:09 -0300

    Initial Commit

---
+++ landmarks/landmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarksApp.swift
+//  landmarks
+//
+//  Created by Fernando Moreno on 12/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct landmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
